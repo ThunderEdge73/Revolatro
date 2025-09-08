2e9d932757f1bfbd12b34c91fb7bf4f11328cab3
PokerThePoker <kowyn865@gmail.com>
2025-09-08 18:41:51 -0300

    red

---
+++ content/jokers.lua
@@ -0,0 +1,33 @@
+SMODS.Joker {
+	key = "red",
+	config = { extra = { Xmult = 1, Xmult_gain = 0.1, count = 0, needed = 5 } },
+	rarity = 1,
+	atlas = "rev_placeholder",
+	pos = { x = 0, y = 0 },
+	cost = 4,
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_gain, card.ability.extra.count, card.ability.extra.needed } }
+	end,
+	calculate = function(self, card, context)
+        if context.before then
+            card.ability.extra.count = card.ability.extra.count + 1
+            if card.ability.extra.count >= card.ability.extra.needed then
+                card.ability.extra.count = 0
+                card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_gain
+                return {
+                    message = "X" .. card.ability.extra.Xmult .. " Mult"
+                }
+			else
+				return {
+					message = card.ability.extra.count .. "/" .. card.ability.extra.needed
+				}
+            end
+        end
+		if context.joker_main then
+			return {
+				Xmult = card.ability.extra.Xmult
+			}
+		end
+	end
+}
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,7 +1,16 @@
 return {
     descriptions = {
         Joker = {
-            
+            j_rev_red = {
+                name = "Red",
+                text = {
+                    "This Joker gains",
+                    "{X:red,C:white}X#2#{} Mult every",
+                    "{C:attention}#4#{} hands played",
+                    "{C:inactive}(#3#/#4#){}",
+                    "{C:inactive}(Currently{} {X:red,C:white}X#1#{} {C:inactive}Mult){}",
+                }
+            }
         }
     },
     misc = {
