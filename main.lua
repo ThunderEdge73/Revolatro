87422144dc73802f8c11b7cd72d1f4761562a18f
Alvin Dong <alvindong9913@gmail.com>
2025-09-08 16:15:07 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 REV = {}
-REV.path = SMODS.current_mod.path
+REV.path = "" .. SMODS.current_mod.path
 
-local content_files = NFS.getDirectoryItems(REV.path .. "mod")
+local content_files = NFS.getDirectoryItems(REV.path .. "content")
 for _, item in ipairs(content_files) do
     local f, err = SMODS.load_file("content/" .. item)
     if err then error(err) elseif f then f() end
