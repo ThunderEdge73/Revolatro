0553ee65bfcbf75febf819c5779abfe15a2b7c0e
PokerThePoker <kowyn865@gmail.com>
2025-09-09 06:29:05 -0300

    orange

--- content/jokers.lua
+++ content/jokers.lua
@@ -4,7 +4,7 @@ SMODS.Joker {
 	rarity = 1,
 	atlas = "rev_placeholder",
 	pos = { x = 0, y = 0 },
-	cost = 4,
+	cost = 5,
 	blueprint_compat = true,
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_gain, card.ability.extra.count, card.ability.extra.needed } }
@@ -24,6 +24,40 @@ SMODS.Joker {
 				}
             end
         end
+		if context.joker_main then
+			return {
+				Xmult = card.ability.extra.Xmult
+			}
+		end
+	end
+}
+
+SMODS.Joker {
+	key = "orange",
+	config = { extra = { Xmult = 1, Xmult_gain = 0.1, count = 0, needed = 3 } },
+	rarity = 1,
+	atlas = "rev_placeholder",
+	pos = { x = 0, y = 0 },
+	cost = 6,
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_gain, card.ability.extra.count, card.ability.extra.needed } }
+	end,
+	calculate = function(self, card, context)
+		if (context.selling_card and context.card.ability.set == "Joker") and not context.blueprint then
+            card.ability.extra.count = card.ability.extra.count + 1
+            if card.ability.extra.count >= card.ability.extra.needed then
+                card.ability.extra.count = 0
+                card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_gain
+                return {
+                    message = "X" .. card.ability.extra.Xmult .. " Mult"
+                }
+			else
+				return {
+					message = card.ability.extra.count .. "/" .. card.ability.extra.needed
+				}
+            end
+		end
 		if context.joker_main then
 			return {
 				Xmult = card.ability.extra.Xmult
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -7,10 +7,20 @@ return {
                     "This Joker gains",
                     "{X:red,C:white}X#2#{} Mult every",
                     "{C:attention}#4#{} hands played",
-                    "{C:inactive}(#3#/#4#){}",
                     "{C:inactive}(Currently{} {X:red,C:white}X#1#{} {C:inactive}Mult){}",
+                    "{C:inactive}(#3#/#4#){}",
                 }
-            }
+            },
+            j_rev_orange = {
+                name = "Orange",
+                text = {
+                    "This Joker gains",
+                    "{X:red,C:white}X#2#{} Mult every",
+                    "{C:attention}#4#{} Jokers sold",
+                    "{C:inactive}(Currently{} {X:red,C:white}X#1#{} {C:inactive}Mult){}",
+                    "{C:inactive}(#3#/#4#){}",
+                }
+            },
         }
     },
     misc = {
